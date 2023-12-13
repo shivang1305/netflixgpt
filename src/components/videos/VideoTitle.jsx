bd0849d9087990eae8fa2b48ece5f4eb05aeff97
Shivang Sharma <shivang@Shivangs-MacBook-Air.local>
2023-12-13 10:00:54 +0530

    fix: video title width is decreased to avoid overlap with the video in bg

--- src/components/videos/VideoTitle.jsx
+++ src/components/videos/VideoTitle.jsx
@@ -4,7 +4,7 @@ import { AiFillPlayCircle, AiOutlineInfoCircle } from "react-icons/ai";
 const VideoTitle = ({ title, overview }) => {
   return (
     <div className="w-screen aspect-video pt-[20%] px-24 absolute text-white bg-gradient-to-tr from-black">
-      <h1 className="text-6xl font-bold">{title}</h1>
+      <h1 className="text-6xl font-bold w-1/3">{title}</h1>
       <p className="py-6 text-lg w-1/3">{overview}</p>
       <div className="flex">
         <button className="bg-white text-black p-4 px-12 text-2xl rounded-lg flex">
